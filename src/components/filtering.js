5efb03120c17d1fada4679e877c431a3edc04fce
Anna Li <anna.li.4725@gmail.com>
2026-04-24 12:36:30 +0500

    release: v1.0.0

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,41 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
+    Object.keys(indexes)                                    // Получаем ключи из объекта
+      .forEach((elementName) => {                        // Перебираем по именам
+        elements[elementName].append(                    // в каждый элемент добавляем опции
+            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
+                      .map(name => {                        // используйте name как значение и текстовое содержимое
+                        const option = document.createElement('option');     // @todo: создать и вернуть тег опции
+                        option.value = name;
+                        option.textContent = name;
+                        return option
+                      })
+        )
+     })
 
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
+        if (action && action.name === 'clear') {
+            const parent = action.parentElement;
+            const input = parent.querySelector('input');
+            if(input) {
+                input.value = '';
+            };
+
+            const field = action.dataset.field;
+            if (field in state) {
+                state[field] = '';
+            }
+
+        }
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
+        // return data; - заменили его на то, что выше
     }
 }
